da640f867d0ca36ad97903fbcf46842a966012fa
Ran Wang <randombazinga@gmail.com>
2019-06-19 10:32:00 -0400

    build tag on graham

--- OpenFOAM-6/buildTag.sh
+++ OpenFOAM-6/buildTag.sh
@@ -8,3 +8,10 @@ find -H "/home/pi/OpenFOAM/OpenFOAM-6" \
      \( -name "*.[HC]" -o -name lnInclude -prune -o -name Doxygen -prune \) | \
 
     $etagsCmd
+
+# etagsCmd="/home/superran/bin/bin/etags -e --extra=+fq --file-scope=no --c-kinds=+p -o .tags/etags -L -"
+
+# find -H "/home/superran/myGitHub/EI-FOAM/OpenFOAM-6" \
+#      \( -name "*.[HC]" -o -name lnInclude -prune -o -name Doxygen -prune \) | \
+
+#     $etagsCmd
