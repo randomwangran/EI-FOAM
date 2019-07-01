d276e5e7552a608a1eedab41e25b01a6413ca69e
Ran Wang <randombazinga@gmail.com>
2019-07-01 14:20:30 -0400

    [vector] inner product

--- OpenFOAM-6/wrTest/wrVectorTest.C
+++ OpenFOAM-6/wrTest/wrVectorTest.C
@@ -25,9 +25,39 @@ using namespace Foam;
 int main(int argc, char *argv[])
 {
 
-  const Vector<int32_t>& a = (1 2 3);
-  const Vector<int32_t>& b = (4 5 6);
+  //-wr    const Vector<int32_t>& a = (1 2 3);
+  //-wr    const Vector<int32_t>& b = (4 5 6);
 
+  //-wr    vector a = (1,2,3);
+  //-wr    vector a = (1,2,3);
+  //-wr    point a = (1, 2, 3);
+  point a;
+  a.x()=1;
+  a.y()=2;
+  a.z()=3;
+
+  point b;
+  b.x()=4;
+  b.y()=5;
+  b.z()=6;
+
+  //-wr    point c;  
+  //-wr    const point& a = (1 2 3);
+  //-wr    const point& b = (4 5 6);
+  //-wr    c = a&b;
+
+  scalar c;
+  c = a & b;
+
+  //-wr    a[1 2 3] dot  b[4 5 6]
+  //-wr    
+  //-wr      = 1*4 +2*5 +3*6
+  //-wr    
+  //-wr      = 4+10+18
+  //-wr    
+  //-wr      = 32
+  
   Info<< "Hi foo!" << endl;
+  Info<<  c << endl;
   return 0;
 }
